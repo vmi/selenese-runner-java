4ecc7e4650e10b891f79f2b1b9ee28e06d7e05e8
Hayato ITO <haya10.ito@gmail.com>
2012-07-19 21:51:28 +0900

    backup and restore systemproperty for test.

--- src/test/java/jp/vmi/selenium/webdriver/FirefoxDriverFactoryTest.java
+++ src/test/java/jp/vmi/selenium/webdriver/FirefoxDriverFactoryTest.java
@@ -6,12 +6,39 @@ import jp.vmi.selenium.selenese.InvalidConfigurationException;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.TemporaryFolder;
 
 public class FirefoxDriverFactoryTest {
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
+
+    /**
+     * backup Systemproperty and restore.
+     */
+    @Rule
+    public ExternalResource systemproperty = new ExternalResource() {
+        String original;
+        final String key = "webdriver.firefox.bin";
+
+        @Override
+        protected void before() throws Throwable {
+            super.before();
+            original = System.getProperty(key);
+            System.setProperty(key, folder.newFolder().getAbsolutePath());
+        }
+
+        @Override
+        protected void after() {
+            if (original == null) {
+                System.clearProperty(key);
+            } else {
+                System.setProperty(key, original);
+            }
+            super.after();
+        }
+    };
 
     @Test(expected = BrowserNotFoundException.class)
     public void firefoxNotFoundIn_webdriver_firefox_bin() throws IOException, InvalidConfigurationException, BrowserNotFoundException {
