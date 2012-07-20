6fbb19ef67664201263c7702a6315356ba0d2336
Hayato ITO <haya10.ito@gmail.com>
2012-07-20 23:09:36 +0900

    add empty selenese script test.

--- src/test/java/jp/vmi/selenium/selenese/RunnerTest.java
+++ src/test/java/jp/vmi/selenium/selenese/RunnerTest.java
@@ -10,6 +10,7 @@ import jp.vmi.selenium.webdriver.DriverOptions.DriverOption;
 
 import org.apache.commons.cli.CommandLine;
 import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class RunnerTest {
 
@@ -19,5 +20,12 @@ public class RunnerTest {
         CommandLine cli = Runner.getCommandLine(new String[] { "--proxy", "proxy.example.com", tmp.getAbsolutePath() });
         DriverOptions opt = new DriverOptions(cli);
         assertEquals("proxy.example.com", opt.get(DriverOption.PROXY));
+    }
+
+    @Test
+    public void emptyFile() throws IOException {
+        File tmp = File.createTempFile("aaa", "test.html");
+        Runner runner = new Runner(new FirefoxDriver());
+        runner.run(tmp.getAbsolutePath());
     }
 }
