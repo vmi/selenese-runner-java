1b564e66fd7a69654b2eb41daf566d8c0bee2324
Hayato ITO <haya10.ito@gmail.com>
2012-08-08 22:50:49 +0900

    IE test skipped by "assumeThat" instead of @ignore tag.

--- src/test/java/jp/vmi/selenium/selenese/CommandRunnerInternetExplorerProxyTest.java
+++ src/test/java/jp/vmi/selenium/selenese/CommandRunnerInternetExplorerProxyTest.java
@@ -2,13 +2,11 @@ package jp.vmi.selenium.selenese;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 
 import jp.vmi.selenium.webdriver.DriverOptions;
 import jp.vmi.selenium.webdriver.DriverOptions.DriverOption;
 import jp.vmi.selenium.webdriver.WebDriverManager;
 
-@Ignore("not yet ready to IE proxy test.")
 public class CommandRunnerInternetExplorerProxyTest extends CommandRunnerInternetExplorerTest {
     static Proxy proxy = new Proxy();
 
