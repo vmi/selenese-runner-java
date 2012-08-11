81f3058d5905248fa62e2825b28e78ac800d088a
Hayato ITO <haya10.ito@gmail.com>
2012-08-11 15:56:11 +0900

    remove unused method.

--- src/test/java/jp/vmi/selenium/selenese/TestUtils.java
+++ src/test/java/jp/vmi/selenium/selenese/TestUtils.java
@@ -2,13 +2,8 @@ package jp.vmi.selenium.selenese;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.Socket;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.UnknownHostException;
-
-import org.junit.Assume;
 
 /**
  * Utility for Test.
@@ -17,21 +12,6 @@ public final class TestUtils {
 
     private TestUtils() {
         // no operation
-    }
-
-    /**
-     * Check proxy.
-     */
-    public static void checkProxy() {
-        // assume that local proxy is available.
-        try {
-            Socket sock = new Socket("127.0.0.1", 18080);
-            sock.close();
-        } catch (UnknownHostException e) {
-            Assume.assumeNoException(e);
-        } catch (IOException e) {
-            Assume.assumeNoException(e);
-        }
     }
 
     /**
