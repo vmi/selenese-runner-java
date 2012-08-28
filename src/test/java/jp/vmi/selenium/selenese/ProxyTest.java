4b2b3ab7e4d23b8342abdd9046050c716039cddd
Hayato ITO <haya10.ito@gmail.com>
2012-08-29 00:36:38 +0900

    refs #29 #30

--- src/test/java/jp/vmi/selenium/selenese/ProxyTest.java
+++ src/test/java/jp/vmi/selenium/selenese/ProxyTest.java
@@ -1,5 +1,6 @@
 package jp.vmi.selenium.selenese;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -14,6 +15,7 @@ public class ProxyTest {
      * Test of start() and kill() continuously.
      */
     @Test
+    @Ignore("ignore by #29")
     public void continuouslyInvoke() {
         for (int i = 0; i < 20; i++) {
             WebrickServer proxy = new Proxy();
@@ -26,6 +28,7 @@ public class ProxyTest {
      * Test of start() and kill().
      */
     @Test(timeout = 10000)
+    @Ignore("ignore by #30")
     public void startAndKill() {
         WebrickServer proxy = new Proxy();
         proxy.start();
