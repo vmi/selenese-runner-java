77c05d235ed2813e372398455d3d22a99735eae6
Hayato ITO <haya10.ito@gmail.com>
2013-01-15 00:17:58 +0900

    add test for #52

--- src/test/java/jp/vmi/selenium/selenese/RunnerTest.java
+++ src/test/java/jp/vmi/selenium/selenese/RunnerTest.java
@@ -4,19 +4,29 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import jp.vmi.selenium.selenese.result.Result;
 import jp.vmi.selenium.webdriver.DriverOptions;
 import jp.vmi.selenium.webdriver.DriverOptions.DriverOption;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 /**
  * Test for {@link Runner}.
  */
 public class RunnerTest {
+    /**
+     * Temprary directory.
+     */
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
 
     /**
      * Test of --proxy option.
@@ -56,6 +66,23 @@ public class RunnerTest {
         runner.setDriver(new HtmlUnitDriver());
         Result result = runner.run("nosuchfile.html");
         assertTrue(result.isFailed());
+    }
+
+    /**
+     * Test of no file.
+     *
+     * @throws IOException exception.
+     */
+    @Test
+    public void ignoreScreenshotCommand() throws IOException {
+        Runner runner = new Runner();
+        //FIXME skip if firefox is not installed
+        runner.setDriver(new FirefoxDriver());
+        runner.setIgnoreScreenshotCommand(true);
+        runner.setScreenshotDir(tmpDir.getRoot().getAbsolutePath());
+        Result result = runner.run(getClass().getResource("RunnerTestIgnoreCapture.html").getPath());
+        assertThat(FileUtils.listFiles(tmpDir.getRoot(), new String[] { "png" }, true).size(), is(0));
+        assertThat(result.isSuccess(), is(true));
     }
 
     /**
