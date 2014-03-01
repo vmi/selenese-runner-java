4cf2f6e236f4c74719bc61743954810bda5d8db9
Motonori IWAMURO <vmi@nifty.com>
2014-03-01 10:11:25 +0900

    The error message is more detailed at throwing exception.

--- src/main/java/jp/vmi/selenium/selenese/result/Error.java
+++ src/main/java/jp/vmi/selenium/selenese/result/Error.java
@@ -1,9 +1,50 @@
 package jp.vmi.selenium.selenese.result;
 
+import java.util.regex.Pattern;
+
 /**
  * Result of error.
  */
 public class Error extends Result {
+
+    private static final Pattern SKIP_RE = Pattern.compile("ByGuice"
+        + "|^com\\.google\\.inject\\."
+        + "|^java\\.lang\\.reflect\\."
+        + "|^sun\\.reflect\\."
+        + "|^org\\.junit\\."
+        + "|^org\\.eclipse\\.jdt\\.");
+
+    private static String getExceptionMessage(Exception e) {
+        StringBuilder result = new StringBuilder("Error: ");
+        String msg = e.getMessage();
+        if (msg != null)
+            result.append(msg);
+        else
+            result.append(e.getClass().getName());
+        result.append(" (");
+        boolean sep = false;
+        for (StackTraceElement stackTrace : e.getStackTrace()) {
+            String className = stackTrace.getClassName();
+            if (className == null || SKIP_RE.matcher(className).find())
+                continue;
+            if (sep)
+                result.append(" / ");
+            result.append(className.replaceFirst("^.*\\.", ""))
+                .append('.')
+                .append(stackTrace.getMethodName());
+            String fileName = stackTrace.getFileName();
+            if (fileName != null) {
+                result.append('(').append(fileName);
+                int lineNumber = stackTrace.getLineNumber();
+                if (lineNumber >= 0)
+                    result.append(':').append(lineNumber);
+                result.append(')');
+            }
+            sep = true;
+        }
+        result.append(")");
+        return result.toString();
+    }
 
     /**
      * Constructor.
@@ -20,7 +61,7 @@ public class Error extends Result {
      * @param e Exception.
      */
     public Error(Exception e) {
-        super(e.getMessage());
+        super(getExceptionMessage(e));
     }
 
     @Override
