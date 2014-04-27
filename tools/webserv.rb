11e585843db2bec20597bd78fb644aaf5e5fc750
Motonori IWAMURO <vmi@nifty.com>
2014-04-27 11:42:49 +0900

    update tool.

--- tools/webserv.rb
+++ tools/webserv.rb
@@ -8,6 +8,16 @@ include WEBrick
 htdocs = File.dirname($0) + "/../src/test/resources/htdocs"
 
 Dir.chdir(htdocs)
-serv = HTTPServer.new(:Port => 80, :DocumentRoot => '.')
+serv = HTTPServer.new(
+  :Port => 80,
+  :DocumentRoot => '.',
+  :RequestCallback => lambda do |req, res|
+    if req.path =~ %r{^/basic/}
+      HTTPAuth.basic_auth(req, res, "REALM") do |username, password|
+        username == 'USERNAME' && password == 'PASSWORD'
+      end
+    end
+  end
+  )
 trap("INT") {serv.shutdown}
 serv.start
