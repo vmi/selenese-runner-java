006b7e5c908200ee313c4e8daf9efef2b2ea9cb0
KIKUCHI Koichiro <koichiro@hataki.jp>
2014-11-07 22:44:56 +0900

    Don't create RollupRules until necessary to be able to work with some Java installations that lack Javascript ScriptEngine support.

--- src/main/java/jp/vmi/selenium/selenese/Runner.java
+++ src/main/java/jp/vmi/selenium/selenese/Runner.java
@@ -81,7 +81,7 @@ public class Runner implements Context, ScreenshotHandler, HighlightHandler, JUn
     private final Deque<CommandListIterator> commandListIteratorStack = new ArrayDeque<CommandListIterator>();
     private VarsMap varsMap = new VarsMap();
     private final CollectionMap collectionMap = new CollectionMap();
-    private final RollupRules rollupRules = new RollupRules();
+    private RollupRules rollupRules; // lazy initialization
     private final Deque<HighlightStyleBackup> styleBackups;
 
     private PageInformation latestPageInformation = PageInformation.EMPTY;
@@ -484,6 +484,9 @@ public class Runner implements Context, ScreenshotHandler, HighlightHandler, JUn
 
     @Override
     public RollupRules getRollupRules() {
+        if (rollupRules == null) {
+            rollupRules = new RollupRules();
+        }
         return rollupRules;
     }
 
