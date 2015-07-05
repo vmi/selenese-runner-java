da3512cbf1e90447732a8b89fcb93e1c022044c4
Motonori IWAMURO <vmi@nifty.com>
2015-07-05 09:16:55 +0900

    remove use of deprecated class.

--- src/main/java/jp/vmi/selenium/selenese/subcommand/WaitForCondition.java
+++ src/main/java/jp/vmi/selenium/selenese/subcommand/WaitForCondition.java
@@ -1,6 +1,6 @@
 package jp.vmi.selenium.selenese.subcommand;
 
-import com.thoughtworks.selenium.Wait;
+import org.openqa.selenium.TimeoutException;
 
 import jp.vmi.selenium.selenese.Context;
 
@@ -10,6 +10,8 @@ import static jp.vmi.selenium.selenese.command.ArgumentType.*;
  * An implementation of the "getEval" method from Selenium.
  */
 public class WaitForCondition extends AbstractSubCommand<Void> {
+
+    private static final long DEFAULT_INTERVAL = 500L;
 
     private static final int ARG_SCRIPT = 0;
     private static final int ARG_TIMEOUT = 1;
@@ -21,25 +23,33 @@ public class WaitForCondition extends AbstractSubCommand<Void> {
         super(VALUE, VALUE);
     }
 
+    private boolean until(Context context, String script) {
+        Object result = context.getEval().eval(context.getWrappedDriver(), script);
+        if (result == null)
+            return false;
+        else if (result instanceof String)
+            return !((String) result).isEmpty();
+        else if (result instanceof Boolean)
+            return (Boolean) result;
+        else
+            return true;
+    }
+
     @Override
     public Void execute(final Context context, String... args) {
-        final String script = args[ARG_SCRIPT];
-        final String timeout = args[ARG_TIMEOUT];
-        new Wait() {
-            @Override
-            public boolean until() {
-                Object result = context.getEval().eval(context.getWrappedDriver(), script);
-                if (result == null) {
-                    return false;
-                } else if (result instanceof String) {
-                    return !"".equals(result);
-                } else if (result instanceof Boolean) {
-                    return (Boolean) result;
-                } else {
-                    return true;
-                }
+        String script = args[ARG_SCRIPT];
+        long timeout = Long.valueOf(args[ARG_TIMEOUT]);
+
+        long end = System.currentTimeMillis() + timeout;
+        while (System.currentTimeMillis() < end) {
+            if (until(context, script))
+                return null;
+            try {
+                Thread.sleep(DEFAULT_INTERVAL);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }.wait("Failed to resolve " + script, Long.valueOf(timeout));
-        return null;
+        }
+        throw new TimeoutException("Failed to resolve " + script);
     }
 }
