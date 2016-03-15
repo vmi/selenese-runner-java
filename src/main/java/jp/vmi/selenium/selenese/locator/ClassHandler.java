babac01bc6b026d65511ea54b9963006c78a6394
IWAMURO Motonori <vmi@nifty.com>
2016-03-16 07:32:06 +0900

    refine ClassHandler.

--- src/main/java/jp/vmi/selenium/selenese/locator/ClassHandler.java
+++ src/main/java/jp/vmi/selenium/selenese/locator/ClassHandler.java
@@ -1,29 +1,27 @@
 package jp.vmi.selenium.selenese.locator;
 
-
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 class ClassHandler implements LocatorHandler {
 
-   @Override
-   public String locatorType() {
-      return "class";
-   }
+    @Override
+    public String locatorType() {
+        return "class";
+    }
 
-   @Override
-   public List<WebElement> handle(WebDriver driver, String arg) {
-      By by;
-      if (StringUtils.contains(arg, StringUtils.SPACE)) {
-         //avoid InvalidSelectorError: Compound class names not permitted
-         by = By.cssSelector("." + StringUtils.replace(arg, StringUtils.SPACE, "."));
-      } else {
-         by = By.className(arg);
-      }
-      return driver.findElements(by);
-   }
+    @Override
+    public List<WebElement> handle(WebDriver driver, String arg) {
+        By by;
+        if (arg != null && arg.indexOf(' ') >= 0) {
+            //avoid InvalidSelectorError: Compound class names not permitted
+            by = By.cssSelector("." + arg.replace(' ', '.'));
+        } else {
+            by = By.className(arg);
+        }
+        return driver.findElements(by);
+    }
 }
