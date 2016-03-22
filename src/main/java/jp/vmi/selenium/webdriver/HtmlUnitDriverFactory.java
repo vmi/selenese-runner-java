9c4f102c7706de5ba85583e27753e8d5e523ead5
IWAMURO Motonori <vmi@nifty.com>
2016-03-23 07:05:47 +0900

    remove explicit dependency on HtmlUnitDriver.

--- src/main/java/jp/vmi/selenium/webdriver/HtmlUnitDriverFactory.java
+++ src/main/java/jp/vmi/selenium/webdriver/HtmlUnitDriverFactory.java
@@ -1,14 +1,16 @@
 package jp.vmi.selenium.webdriver;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
- * Factory of {@link HtmlUnitDriver}.
+ * Factory of HtmlUnitDriver.
  */
 public class HtmlUnitDriverFactory extends WebDriverFactory {
+
+    private static final String HTML_UNIT_DRIVER = "org.openqa.selenium.htmlunit.HtmlUnitDriver";
 
     /**
      * Constructor.
@@ -22,9 +24,16 @@ public class HtmlUnitDriverFactory extends WebDriverFactory {
         DesiredCapabilities caps = setupProxy(DesiredCapabilities.htmlUnit(), driverOptions);
         caps.setJavascriptEnabled(true);
         caps.merge(driverOptions.getCapabilities());
-        HtmlUnitDriver driver = new HtmlUnitDriver(caps);
-        setInitialWindowSize(driver, driverOptions);
-        return driver;
+        try {
+            WebDriver driver = (WebDriver) Class.forName(HTML_UNIT_DRIVER).getConstructor(Capabilities.class).newInstance(caps);
+            // HtmlUnitDriver driver = new HtmlUnitDriver(caps);
+            setInitialWindowSize(driver, driverOptions);
+            return driver;
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
