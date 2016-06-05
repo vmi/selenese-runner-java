ccafe6979c65e2af1650493c7e7a0ead24b34e90
IWAMURO Motonori <vmi@nifty.com>
2016-06-05 11:13:13 +0900

    add test case for issue #199.

---
+++ src/test/java/jp/vmi/selenium/selenese/utils/WindowSelectorTest.java
@@ -0,0 +1,19 @@
+package jp.vmi.selenium.selenese.utils;
+
+import org.junit.Test;
+
+import jp.vmi.selenium.selenese.result.Success;
+import jp.vmi.selenium.testutils.DriverDependentTestCaseTestBase;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+@SuppressWarnings("javadoc")
+public class WindowSelectorTest extends DriverDependentTestCaseTestBase {
+
+    @Test
+    public void testSelectWindow() {
+        execute("testcase_issue199");
+        assertThat(result, is(instanceOf(Success.class)));
+    }
+}
