95966ed61f2e20e10d31e068c075a3fe1028f505
Alexander Trofimov <atrofimov@adyax.com>
2017-03-21 23:21:40 +0200

    Update ClickOnRandomSelectElement

--- src/main/java/jp/vmi/selenium/selenese/command/ClickOnRandomSelectElement.java
+++ src/main/java/jp/vmi/selenium/selenese/command/ClickOnRandomSelectElement.java
@@ -2,11 +2,13 @@ package jp.vmi.selenium.selenese.command;
 
 import jp.vmi.selenium.selenese.Context;
 import jp.vmi.selenium.selenese.result.Result;
-import org.apache.commons.lang3.RandomStringUtils;
+import jp.vmi.selenium.selenese.result.Success;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.Random;
 
 import static jp.vmi.selenium.selenese.command.ArgumentType.LOCATOR;
 import static jp.vmi.selenium.selenese.result.Success.SUCCESS;
@@ -24,25 +26,36 @@ public class ClickOnRandomSelectElement extends AbstractCommand{
     ClickOnRandomSelectElement(int index, String name, String... args) {
         super(index, name, args, LOCATOR);
     }
+    //        var xPathRes = this.browserbot.getDocument().evaluate(locator, this.browserbot.getDocument(), null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+//        if (xPathRes.snapshotLength > 0) {
+//            var randomNum = Math.floor(2 + Math.random() * (xPathRes.snapshotLength - 1));
+//            var randomElem = locator + '[' + randomNum + ']';
+//            this.doClickAt(randomElem);
+//        } else throw new SeleniumError('Locator does not exist');
+//    };
 
+    // Fix me
     @Override
     protected Result executeImpl(Context context, String... curArgs) {
-
         String locator = curArgs[ARG_LOCATOR];
         WebDriver driver = context.getWrappedDriver();
         List<WebElement> elements = context.getElementFinder().findElements(driver, locator);
-        if (elements.size() > 0)
-            System.out.println(elements.size());
-        String value = RandomStringUtils.random(1, false, true);
-        System.out.println(value);
-
-//        try {
-////            element.click();
-////            return SUCCESS;
-//        } catch (ElementNotVisibleException e) {
-////            context.executeScript("arguments[0].click()", element);
-////            return new Success("Success (the element is not visible)");
-//        }
+        int randomNum = 0;
+        if (elements.size() > 0) {
+            randomNum = new Random().nextInt((elements.size() - 1) + 1);
+            WebElement element = context.getElementFinder().findElement(driver, locator + '[' + randomNum + ']');
+            context.getJSLibrary().replaceAlertMethod(driver, element);
+            try {
+                element.click();
+                return SUCCESS;
+            } catch (ElementNotVisibleException e) {
+                context.executeScript("arguments[0].click()", element);
+                return new Success("Success (the element is not visible)");
+            }
+        }
         return SUCCESS;
     }
 }
+
+
+
