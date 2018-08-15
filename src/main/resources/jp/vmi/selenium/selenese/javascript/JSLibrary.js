2dfb1e6cfdec66fdf365df3d35d4a20c656bcf49
KIKUCHI Koichiro <koichiro@rworks.jp>
2018-08-15 18:42:43 +0900

    Improve old IE support.

--- src/main/resources/jp/vmi/selenium/selenese/javascript/JSLibrary.js
+++ src/main/resources/jp/vmi/selenium/selenese/javascript/JSLibrary.js
@@ -27,34 +27,61 @@
 //
 
 function replaceAlertMethod(element) {
-  window.localStorage.setItem('__webdriverAlerts', JSON.stringify([]));
-  window.alert = function(msg) {
-    var alerts = JSON.parse(window.localStorage.getItem('__webdriverAlerts'));
-    alerts.push(msg);
-    window.localStorage.setItem('__webdriverAlerts', JSON.stringify(alerts));
-  };
-  window.localStorage.setItem('__webdriverConfirms', JSON.stringify([]));
-  if (!('__webdriverNextConfirm' in window.localStorage))
-    window.localStorage.setItem('__webdriverNextConfirm', JSON.stringify(true));
-  window.confirm = function(msg) {
-    var confirms = JSON.parse(window.localStorage.getItem('__webdriverConfirms'));
-    confirms.push(msg);
-    window.localStorage.setItem('__webdriverConfirms', JSON.stringify(confirms));
-    var res = JSON.parse(window.localStorage.getItem('__webdriverNextConfirm'));
-    window.localStorage.setItem('__webdriverNextConfirm', JSON.stringify(true));
-    return res;
-  };
-  window.localStorage.setItem('__webdriverPrompts', JSON.stringify([]));
-  if (!('__webdriverNextPrompt' in window.localStorage))
-    window.localStorage.setItem('__webdriverNextPrompt', JSON.stringify(""));
-  window.prompt = function(msg) {
-    var prompts = JSON.parse(window.localStorage.getItem('__webdriverPrompts'));
-    prompts.push(msg);
-    window.localStorage.setItem('__webdriverPrompts', JSON.stringify(prompts));
-    var res = JSON.parse(window.localStorage.getItem('__webdriverNextPrompt'));
-    window.localStorage.setItem('__webdriverNextPrompt', JSON.stringify(""));
-    return res;
-  };
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    window.localStorage.setItem('__webdriverAlerts', JSON.stringify([]));
+    window.alert = function(msg) {
+      var alerts = JSON.parse(window.localStorage.getItem('__webdriverAlerts'));
+      alerts.push(msg);
+      window.localStorage.setItem('__webdriverAlerts', JSON.stringify(alerts));
+    };
+    window.localStorage.setItem('__webdriverConfirms', JSON.stringify([]));
+    if (!('__webdriverNextConfirm' in window.localStorage))
+      window.localStorage.setItem('__webdriverNextConfirm', JSON.stringify(true));
+    window.confirm = function(msg) {
+      var confirms = JSON.parse(window.localStorage.getItem('__webdriverConfirms'));
+      confirms.push(msg);
+      window.localStorage.setItem('__webdriverConfirms', JSON.stringify(confirms));
+      var res = JSON.parse(window.localStorage.getItem('__webdriverNextConfirm'));
+      window.localStorage.setItem('__webdriverNextConfirm', JSON.stringify(true));
+      return res;
+    };
+    window.localStorage.setItem('__webdriverPrompts', JSON.stringify([]));
+    if (!('__webdriverNextPrompt' in window.localStorage))
+      window.localStorage.setItem('__webdriverNextPrompt', JSON.stringify(""));
+    window.prompt = function(msg) {
+      var prompts = JSON.parse(window.localStorage.getItem('__webdriverPrompts'));
+      prompts.push(msg);
+      window.localStorage.setItem('__webdriverPrompts', JSON.stringify(prompts));
+      var res = JSON.parse(window.localStorage.getItem('__webdriverNextPrompt'));
+      window.localStorage.setItem('__webdriverNextPrompt', JSON.stringify(""));
+      return res;
+    };
+  } else {
+    window.__webdriverAlerts = [];
+    window.alert = function(msg) { window.__webdriverAlerts.push(msg); };
+    window.__webdriverConfirms = [];
+    if (typeof window.__webdriverNextConfirm === 'undefined')
+      window.__webdriverNextConfirm = true;
+    window.confirm = function(msg) {
+      window.__webdriverConfirms.push(msg);
+      var res = window.__webdriverNextConfirm;
+      window.__webdriverNextConfirm = true;
+      return res;
+    };
+    window.__webdriverPrompts = [];
+    if (typeof window.__webdriverNextPrompt === 'undefined')
+      window.__webdriverNextPrompt = true;
+    window.prompt = function(msg, def) {
+      window.__webdriverPrompts.push(msg || def);
+      var res = window.__webdriverNextPrompt;
+      window.__webdriverNextPrompt = true;
+      return res;
+    };
+  }
   var fw;
   if (element && (fw = element.ownerDocument.defaultView) && fw != window) {
     fw.alert = window.alert;
@@ -64,71 +91,140 @@ function replaceAlertMethod(element) {
 }
 
 function getNextAlert() {
-  if (!('__webdriverAlerts' in window.localStorage))
-    return null;
-  var alerts = JSON.parse(window.localStorage.getItem('__webdriverAlerts'));
-  if (! alerts)
-    return null;
-  var msg = alerts.shift();
-  window.localStorage.setItem('__webdriverAlerts', JSON.stringify(alerts));
-  if (msg)
-    msg = msg.replace(/\n/g, ' ');
-  return msg;
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    if (!('__webdriverAlerts' in window.localStorage))
+      return null;
+    var alerts = JSON.parse(window.localStorage.getItem('__webdriverAlerts'));
+    if (! alerts)
+      return null;
+    var msg = alerts.shift();
+    window.localStorage.setItem('__webdriverAlerts', JSON.stringify(alerts));
+    if (msg)
+      msg = msg.replace(/\n/g, ' ');
+    return msg;
+  } else {
+    if (!window.__webdriverAlerts) { return null }
+    var t = window.__webdriverAlerts.shift();
+    if (t) { t = t.replace(/\\n/g, ' '); }
+    return t;
+  }
 }
 
 function isAlertPresent() {
-  if (!('__webdriverAlerts' in window.localStorage))
-    return false;
-  var alerts = JSON.parse(window.localStorage.getItem('__webdriverAlerts'));
-  return alerts && alerts.length > 0;
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    if (!('__webdriverAlerts' in window.localStorage))
+      return false;
+    var alerts = JSON.parse(window.localStorage.getItem('__webdriverAlerts'));
+    return alerts && alerts.length > 0;
+  } else {
+    return window.__webdriverAlerts && window.__webdriverAlerts.length > 0;
+  }
 }
 
 function setNextConfirmationState(state) {
-  window.localStorage.setItem('__webdriverNextConfirm', JSON.stringify(state));
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    window.localStorage.setItem('__webdriverNextConfirm', JSON.stringify(state));
+  } else {
+    window.__webdriverNextConfirm = state;
+  }
 }
 
 function getNextConfirmation() {
-  if (!('__webdriverConfirms' in window.localStorage))
-    return null;
-  var confirms = JSON.parse(window.localStorage.getItem('__webdriverConfirms'));
-  if (! confirms)
-    return null;
-  var msg = confirms.shift();
-  window.localStorage.setItem('__webdriverConfirms', JSON.stringify(confirms));
-  if (msg)
-    msg = msg.replace(/\n/g, ' ');
-  return msg;
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    if (!('__webdriverConfirms' in window.localStorage))
+      return null;
+    var confirms = JSON.parse(window.localStorage.getItem('__webdriverConfirms'));
+    if (! confirms)
+      return null;
+    var msg = confirms.shift();
+    window.localStorage.setItem('__webdriverConfirms', JSON.stringify(confirms));
+    if (msg)
+      msg = msg.replace(/\n/g, ' ');
+    return msg;
+  } else {
+    if (!window.__webdriverConfirms) { return null; }
+    return window.__webdriverConfirms.shift();
+  }
 }
 
 function isConfirmationPresent() {
-  if (!('__webdriverConfirms' in window.localStorage))
-    return false;
-  var confirms = JSON.parse(window.localStorage.getItem('__webdriverConfirms'));
-  return confirms && confirms.length > 0;
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    if (!('__webdriverConfirms' in window.localStorage))
+      return false;
+    var confirms = JSON.parse(window.localStorage.getItem('__webdriverConfirms'));
+    return confirms && confirms.length > 0;
+  } else {
+    return window.__webdriverConfirms && window.__webdriverConfirms.length > 0;
+  }
 }
 
 function answerOnNextPrompt(answer) {
-  window.localStorage.setItem('__webdriverNextPrompt', JSON.stringify(answer));
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    window.localStorage.setItem('__webdriverNextPrompt', JSON.stringify(answer));
+  } else {
+    window.__webdriverNextPrompt = answer;
+  }
 }
 
 function getNextPrompt() {
-  if (!('__webdriverPrompts' in window.localStorage))
-    return null;
-  var prompts = JSON.parse(window.localStorage.getItem('__webdriverPrompts'));
-  if (!prompts)
-    return null;
-  var msg = prompts.shift();
-  window.localStorage.setItem('__webdriverPrompts', JSON.stringify(prompts));
-  if (msg)
-    msg = msg.replace(/\n/g, ' ');
-  return msg;
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    if (!('__webdriverPrompts' in window.localStorage))
+      return null;
+    var prompts = JSON.parse(window.localStorage.getItem('__webdriverPrompts'));
+    if (!prompts)
+      return null;
+    var msg = prompts.shift();
+    window.localStorage.setItem('__webdriverPrompts', JSON.stringify(prompts));
+    if (msg)
+      msg = msg.replace(/\n/g, ' ');
+    return msg;
+  } else {
+    if (!window.__webdriverPrompts) { return null; }
+    return window.__webdriverPrompts.shift();
+  }
 }
 
 function isPromptPresent() {
-  if (!('__webdriverPrompts' in window.localStorage))
-    return false;
-  var prompts = JSON.parse(window.localStorage.getItem('__webdriverPrompts'));
-  return prompts && prompts.length > 0;
+  var canUseLocalStorage = false;
+  try { canUseLocalStorage = !!window.localStorage; } catch(ex) { /* probe failed */ }
+  var canUseJSON = false;
+  try { canUseJSON = !!JSON; } catch(ex) { /* probe failed */ }
+  if (canUseLocalStorage && canUseJSON) {
+    if (!('__webdriverPrompts' in window.localStorage))
+      return false;
+    var prompts = JSON.parse(window.localStorage.getItem('__webdriverPrompts'));
+    return prompts && prompts.length > 0;
+  } else {
+    return window.__webdriverPrompts && window.__webdriverPrompts.length > 0;
+  }
 }
 
 //
@@ -245,8 +341,12 @@ function getTable(table, row, col) {
 
 function getText(element) {
   function isVisible(element) {
-    var style = getComputedStyle(element);
+    var style = (typeof getComputedStyle !== 'undefined') ? getComputedStyle(element) : element.currentStyle
     return style.display !== 'none' && style.visibility !== 'hidden';
+  }
+  function trim(text) {
+    // String.replace() version of trim obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Polyfill
+    return String.prototype.trim ? text.trim() : text.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
   }
   function getTextInternal(element) {
     var texts = [];
@@ -274,12 +374,12 @@ function getText(element) {
         // skip
         break;
       default:
-        var text = node.textContent.replace(/[\u0000-\u0020]+/g, " ");
+        var text = (node.textContent || node.innerText || node.nodeValue).replace(/[\u0000-\u0020]+/g, " ");
         texts.push(text);
         break;
       }
     }
-    return texts.join("").replace(/ +/g, " ").trim();
+    return trim(texts.join("").replace(/ +/g, " "));
   }
   return getTextInternal(element);
 }
