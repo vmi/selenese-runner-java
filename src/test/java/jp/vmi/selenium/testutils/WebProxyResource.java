30982a03af10c0c487a4c9a02f051ea08594d2a2
IWAMURO Motonori <vmi@nifty.com>
2018-12-30 03:20:40 +0900

    replace proxy implementation.

--- src/test/java/jp/vmi/selenium/testutils/WebProxyResource.java
+++ src/test/java/jp/vmi/selenium/testutils/WebProxyResource.java
@@ -4,15 +4,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 import org.junit.rules.ExternalResource;
-import org.littleshoot.proxy.HttpFilters;
-import org.littleshoot.proxy.HttpFiltersAdapter;
-import org.littleshoot.proxy.HttpFiltersSourceAdapter;
-import org.littleshoot.proxy.HttpProxyServer;
-import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 import org.openqa.selenium.net.PortProber;
-
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.HttpRequest;
 
 /**
  * Web proxy resource class for test.
@@ -24,39 +16,31 @@ public class WebProxyResource extends ExternalResource {
     /** Dummy URI for proxy test */
     public static final String DUMMY_URI = "http://dummy.example.com/";
 
-    private final int proxyPort;
-    private final HttpProxyServer proxyServer;
-    private InetAddress localHost;
+    private int proxyPort;
     private int localPort;
     private int count;
+    private final ProxyServer proxyServer;
 
     /**
      * Constructor.
      */
     public WebProxyResource() {
-        localHost = InetAddress.getLoopbackAddress();
+        InetAddress localHost = InetAddress.getLoopbackAddress();
         proxyPort = PortProber.findFreePort();
-        proxyServer = DefaultHttpProxyServer.bootstrap()
-            .withPort(proxyPort)
-            .withFiltersSource(new HttpFiltersSourceAdapter() {
-                @Override
-                public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
-                    return new HttpFiltersAdapter(originalRequest) {
-                        @Override
-                        public InetSocketAddress proxyToServerResolutionStarted(String resolvingServerHostAndPort) {
-                            String[] sp = resolvingServerHostAndPort.split(":", 2);
-                            if (!sp[0].endsWith(".example.com"))
-                                return null;
-                            synchronized (WebProxyResource.this) {
-                                count++;
-                            }
-                            // redirect to local web server from dummy URI.
-                            return new InetSocketAddress(localHost, localPort);
-                        }
-                    };
+        proxyServer = ProxyServer.builder(proxyPort)
+            .withProxyToServerResolutionStarted((context, resolvingServerHostAndPort) -> {
+                String[] sp = resolvingServerHostAndPort.split(":", 2);
+                if (!sp[0].endsWith(".example.com"))
+                    return null;
+                synchronized (WebProxyResource.this) {
+                    count++;
                 }
-            }).start();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> proxyServer.stop()));
+                // redirect to local web server from dummy URI.
+                return new InetSocketAddress(localHost, localPort);
+            })
+            .useShutdownHook(true)
+            .build();
+        proxyServer.start();
     }
 
     /**
