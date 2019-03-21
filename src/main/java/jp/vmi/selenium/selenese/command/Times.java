ee3ad3dbad34025907787b32f314bf7a4547b16b
IWAMURO Motonori <vmi@nifty.com>
2019-03-21 17:23:42 +0900

    improve compatibility with Selenium IDE.

--- src/main/java/jp/vmi/selenium/selenese/command/Times.java
+++ src/main/java/jp/vmi/selenium/selenese/command/Times.java
@@ -2,6 +2,7 @@ package jp.vmi.selenium.selenese.command;
 
 import jp.vmi.selenium.selenese.Context;
 import jp.vmi.selenium.selenese.FlowControlState;
+import jp.vmi.selenium.selenese.result.Error;
 import jp.vmi.selenium.selenese.result.Result;
 import jp.vmi.selenium.selenese.result.Success;
 
@@ -13,22 +14,17 @@ import static jp.vmi.selenium.selenese.command.ArgumentType.*;
 public class Times extends BlockStartImpl {
 
     private static final int ARG_TIMES = 0;
+    private static final int ARG_LOOP_LIMIT = 1;
+
+    private static final String LOOP_LIMIT_MESSAGE = "Max retry limit exceeded (count=%d/limit=%d). To override it, specify a new limit in the value input field";
 
     private static class TimesState implements FlowControlState {
 
-        private final long times;
-        private long count = 1;
-
-        TimesState(long times) {
-            this.times = times;
-        }
+        private boolean isAlreadyFinished = false;
+        private long count = 0;
 
         void incrementCount() {
             count++;
-        }
-
-        long getTimes() {
-            return times;
         }
 
         long getCount() {
@@ -37,12 +33,24 @@ public class Times extends BlockStartImpl {
 
         @Override
         public boolean isAlreadyFinished() {
-            return count > times;
+            return isAlreadyFinished;
+        }
+
+        boolean hasReachedTimes(long times) {
+            if (!isAlreadyFinished && count > times)
+                isAlreadyFinished = true;
+            return isAlreadyFinished;
+        }
+
+        boolean hasReachedLoopLimit(long loopLimit) {
+            if (isAlreadyFinished || loopLimit == Long.MAX_VALUE)
+                return false;
+            return count > loopLimit;
         }
     }
 
     Times(int index, String name, String... args) {
-        super(index, name, args, VALUE);
+        super(index, name, args, VALUE, VALUE);
     }
 
     @Override
@@ -50,25 +58,42 @@ public class Times extends BlockStartImpl {
         return true;
     }
 
+    private static long getLongValue(Context context, String s) {
+        return ((Number) context.executeScript("return (" + s + ")")).longValue();
+    }
+
+    private static long getLoopLimit(Context context, String[] args, int index) {
+        if (index >= args.length)
+            return Long.MAX_VALUE;
+        String s = args[index];
+        if (s == null || s.isEmpty())
+            return Long.MAX_VALUE;
+        return getLongValue(context, s);
+    }
+
     @Override
     protected Result executeImpl(Context context, String... curArgs) {
+        long times = getLongValue(context, curArgs[ARG_TIMES]);
+        long loopLimit = getLoopLimit(context, curArgs, ARG_LOOP_LIMIT);
         TimesState state = context.getFlowControlState(this);
-        if (state != null && !state.isAlreadyFinished()) {
-            state.incrementCount();
-            if (state.isAlreadyFinished()) {
-                context.getCommandListIterator().jumpTo(blockEnd);
-                return new Success("Finished " + state.getTimes() + " times repitition");
-            }
-        } else {
-            long times = ((Number) context.executeScript("return (" + curArgs[ARG_TIMES] + ")")).longValue();
-            if (times <= 0) {
-                context.setFlowControlState(this, new TimesState(0));
-                context.getCommandListIterator().jumpTo(blockEnd);
-                return new Success("Skip: times is " + times);
-            }
-            state = new TimesState(times);
+        if (state == null || state.isAlreadyFinished) {
+            state = new TimesState();
             context.setFlowControlState(this, state);
         }
-        return new Success(String.format("Times: %d/%d", state.getCount(), state.getTimes()));
+        state.incrementCount();
+        // Selenium IDE the decision of loop limit first.
+        if (state.hasReachedLoopLimit(loopLimit)) {
+            return new Error(String.format(LOOP_LIMIT_MESSAGE, state.getCount(), loopLimit));
+        } else if (state.hasReachedTimes(times)) {
+            context.getCommandListIterator().jumpTo(blockEnd);
+            return new Success("Finished " + times + " times repitition");
+        }
+
+        String msg;
+        if (loopLimit != Long.MAX_VALUE)
+            msg = String.format("Times (count=%d/times=%d/limit=%d)", state.getCount(), times, loopLimit);
+        else
+            msg = String.format("Times (count=%d/times=%d)", state.getCount(), times);
+        return new Success(msg);
     }
 }
--- src/main/java/jp/vmi/selenium/selenese/command/While.java
+++ src/main/java/jp/vmi/selenium/selenese/command/While.java
@@ -2,6 +2,7 @@ package jp.vmi.selenium.selenese.command;
 
 import jp.vmi.selenium.selenese.Context;
 import jp.vmi.selenium.selenese.FlowControlState;
+import jp.vmi.selenium.selenese.result.Error;
 import jp.vmi.selenium.selenese.result.Result;
 import jp.vmi.selenium.selenese.result.Success;
 
@@ -13,10 +14,22 @@ import static jp.vmi.selenium.selenese.command.ArgumentType.*;
 public class While extends BlockStartImpl {
 
     private static final int ARG_CONDITION = 0;
+    private static final int ARG_LOOP_LIMIT = 1;
+
+    private static final String LOOP_LIMIT_MESSAGE = "Max retry limit exceeded (count=%d/limit=%d). To override it, specify a new limit in the value input field";
 
     private static class WhileState implements FlowControlState {
 
         private boolean isAlreadyFinished = false;
+        private long count = 0;
+
+        void incrementCount() {
+            count++;
+        }
+
+        long getCount() {
+            return count;
+        }
 
         @Override
         public boolean isAlreadyFinished() {
@@ -26,10 +39,16 @@ public class While extends BlockStartImpl {
         public void setAlreadyFinished(boolean isAlreadyFinished) {
             this.isAlreadyFinished = isAlreadyFinished;
         }
+
+        boolean hasReachedLoopLimit(long loopLimit) {
+            if (isAlreadyFinished || loopLimit == Long.MAX_VALUE)
+                return false;
+            return count > loopLimit;
+        }
     }
 
     While(int index, String name, String... args) {
-        super(index, name, args, VALUE);
+        super(index, name, args, VALUE, VALUE);
     }
 
     @Override
@@ -37,19 +56,41 @@ public class While extends BlockStartImpl {
         return true;
     }
 
+    private static long getLongValue(Context context, String s) {
+        return ((Number) context.executeScript("return (" + s + ")")).longValue();
+    }
+
+    private static long getLoopLimit(Context context, String[] args, int index) {
+        if (index >= args.length)
+            return Long.MAX_VALUE;
+        String s = args[index];
+        if (s == null || s.isEmpty())
+            return Long.MAX_VALUE;
+        return getLongValue(context, s);
+    }
+
     @Override
     protected Result executeImpl(Context context, String... curArgs) {
+        long loopLimit = getLoopLimit(context, curArgs, ARG_LOOP_LIMIT);
         WhileState state = context.getFlowControlState(this);
         if (state == null) {
             state = new WhileState();
             context.setFlowControlState(this, state);
         }
-        if (!context.isTrue(curArgs[ARG_CONDITION])) {
+        state.incrementCount();
+        if (state.hasReachedLoopLimit(loopLimit)) {
+            return new Error(String.format(LOOP_LIMIT_MESSAGE, state.getCount(), loopLimit));
+        } else if (!context.isTrue(curArgs[ARG_CONDITION])) {
             state.setAlreadyFinished(true);
             context.getCommandListIterator().jumpTo(blockEnd);
             return new Success("Break");
         } else {
-            return new Success("Continue");
+            String msg;
+            if (loopLimit != Long.MAX_VALUE)
+                msg = String.format("Continue (count=%d/limit=%d)", state.getCount(), loopLimit);
+            else
+                msg = String.format("Continue (count=%d)", state.getCount());
+            return new Success(msg);
         }
     }
 }
