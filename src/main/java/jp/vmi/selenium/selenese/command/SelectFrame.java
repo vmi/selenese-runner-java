f1f414e071f073d06bb07ab505ffa01fc29fc4e6
IWAMURO Motonori <vmi@nifty.com>
2019-06-23 13:23:18 +0900

    in `selectFrame` command, retry until timeout if specified frame is not found.

--- src/main/java/jp/vmi/selenium/selenese/command/SelectFrame.java
+++ src/main/java/jp/vmi/selenium/selenese/command/SelectFrame.java
@@ -1,7 +1,14 @@
 package jp.vmi.selenium.selenese.command;
 
+import org.openqa.selenium.NoSuchFrameException;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jp.vmi.selenium.selenese.Context;
+import jp.vmi.selenium.selenese.locator.WebDriverElementFinder;
 import jp.vmi.selenium.selenese.result.Result;
+import jp.vmi.selenium.selenese.utils.Wait;
 
 import static jp.vmi.selenium.selenese.command.ArgumentType.*;
 import static jp.vmi.selenium.selenese.result.Success.*;
@@ -11,7 +18,12 @@ import static jp.vmi.selenium.selenese.result.Success.*;
  */
 public class SelectFrame extends AbstractCommand {
 
+    private static final Logger log = LoggerFactory.getLogger(SelectFrame.class);
+
     private static final int ARG_LOCATOR = 0;
+
+    private static long RETRY_INTERVAL = 100L; // ms
+    private static long MILLI_TO_NANO = 1000L * 1000L;
 
     SelectFrame(int index, String name, String... args) {
         super(index, name, args, LOCATOR);
@@ -20,7 +32,21 @@ public class SelectFrame extends AbstractCommand {
     @Override
     protected Result executeImpl(Context context, String... curArgs) {
         String locator = curArgs[ARG_LOCATOR];
-        context.getElementFinder().selectFrame(context.getWrappedDriver(), locator);
-        return SUCCESS;
+        WebDriver driver = context.getWrappedDriver();
+        WebDriverElementFinder elementFinder = context.getElementFinder();
+        long timeout = context.getTimeout() * MILLI_TO_NANO; // ns
+        long start = System.nanoTime();
+        while (true) {
+            try {
+                elementFinder.selectFrame(driver, locator);
+                return SUCCESS;
+            } catch (NoSuchFrameException e) {
+                if (System.nanoTime() - start > timeout) {
+                    log.warn("Timed out select frame: {}", locator);
+                    throw e;
+                }
+            }
+            Wait.sleep(RETRY_INTERVAL);
+        }
     }
 }
